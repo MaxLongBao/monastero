c354282cf3a888be58bb883794be478d47c9cdd6
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2022-10-07 13:39:27 +0100

    frame four done

--- src/components/FourthFrame/index.jsx
+++ src/components/FourthFrame/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import google_pin from '../../assets/google_pin.png';
 
 import './styles.css';
 
 function FourthFrame() {
   return (
     <div className="fourth-frame">
-      <div className="pin">
-        <img src="" alt="" />
-      </div>
-      <div className="fourth-frame-text">
-        <p>The meetup will take place at the Convent of the Capuchin Friars (Monastero dei frati cappuccini) in the village of Grottaglie in a temporary music studio.</p>
+      <div className="frame-four-content">
+        <div className="pin">
+          <img src={google_pin} alt="google pin" />
+        </div>
+        <div className="fourth-frame-text">
+          <p>The meetup will take place at the Convent of the Capuchin Friars (Monastero dei frati cappuccini) in the village of Grottaglie in a temporary music studio.</p>
+        </div>
       </div>
     </div>
   );
